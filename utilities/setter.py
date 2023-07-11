7e94be8d4278b342de84a4fc9da9d797fbba2350
fedepacher <fede_pacher@hotmail.com>
2023-07-11 19:35:49 -0300

    fix(#17): fix write env variable to file

--- utilities/setter.py
+++ utilities/setter.py
@@ -6,7 +6,7 @@ from base64 import b64decode
 def main():
     """Get service account key for github action"""
     key = os.environ.get('SERVICE_ACCOUNT_KEY')
-    with open('path.json', 'w') as json_file:
+    with open('path.json', 'wb') as json_file:
         json_file.write(b64decode(b64decode(key).decode()))
     print(os.path.realpath('path.json'))
 
