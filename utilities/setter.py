1de818b0418ad90d360fd0d210b87951e51a5a37
fedepacher <fede_pacher@hotmail.com>
2023-07-12 01:27:57 -0300

    refactor(#17): refactor get environment variable

--- utilities/setter.py
+++ utilities/setter.py
@@ -6,8 +6,8 @@ from base64 import b64decode
 def main():
     """Get service account key for github action"""
     key = os.environ.get('SERVICE_ACCOUNT_KEY')
-    with open('path.json', 'wb') as json_file:
-        json_file.write(b64decode(b64decode(key).decode()))
+    with open('path.json','w') as json_file:
+        json_file.write(b64decode(key).decode())
     print(os.path.realpath('path.json'))
 
 
